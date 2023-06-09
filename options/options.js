a181454aa39835ca834159a68628d83f3d8257d2
Onochie Onochie <onochieo833@gmail.com>
2023-06-09 05:56:14 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "love-baby-41381",
   url: "https://love-baby-41381.botics.co",
   api: "https://love-baby-41381.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
