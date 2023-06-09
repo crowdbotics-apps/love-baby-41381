ef0ce8183fe8c567b594f31ff80d2f1ed516bc33
Crowdbotics <team@crowdbotics.com>
2023-06-09 04:52:32 +0000

    Initial commit.

---
+++ backend/love_baby_41381/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for love_baby_41381 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'love_baby_41381.settings')
+
+application = get_wsgi_application()
